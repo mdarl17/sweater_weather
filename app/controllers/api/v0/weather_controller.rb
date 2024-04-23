026a3490b175941b8e671c3604ebc1b9394c17b5
Matt Darlington <matthewsdarlington@gmail.com>
2024-04-23 10:49:24 -0600

    Move weather controller from v1 to v0 per reqs

---
+++ app/controllers/api/v0/weather_controller.rb
@@ -0,0 +1,12 @@
+class Api::V0::WeatherController < ApplicationController
+	# def show
+	# 	facade = WeatherFacade.new
+	# 	weather = facade.weather(params[:location])
+	# 	render json: WeatherSerializer.new(weather)
+	# end
+
+	def forecast
+		facade = WeatherFacade.new(q: params[:q], aqi: params[:aqi]).forecast
+		render json: WeatherSerializer.new(weather_data)
+	end
+end
--- app/controllers/api/v1/weather_controller.rb
+++
@@ -1,7 +0,0 @@
-class Api::V1::WeatherController < ApplicationController
-	def show
-		facade = WeatherFacade.new
-		weather = facade.weather(params[:location])
-		render json: WeatherSerializer.new(weather)
-	end
-end
