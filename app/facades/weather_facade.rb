7ae5f7c1705db6b9f35f23cd9abdc0ba5ac28506
Matt Darlington <matthewsdarlington@gmail.com>
2024-04-23 12:10:43 -0600

    New weather facade class to clean query params (remove whitespace, downcase); return weather poros to serializer

---
+++ app/facades/weather_facade.rb
@@ -0,0 +1,19 @@
+class WeatherFacade
+	def initialize(search_params)
+		@service = WeatherService.new
+		@location = search_params[:q]
+		@air_quality = search_params[:aqi]
+	end
+
+	def forecast
+		loc_array = @location.delete(" ").split(",")
+		city = loc_array[0].downcase
+		state = loc_array[1].downcase
+		response = @service.get_forecast(location: "#{city},#{state}", air_quality: @air_quality)
+		weather_poro = make_weather_poro(response)
+	end
+
+	def make_weather_poro(weather_data)
+		wp = WeatherPoro.new(weather_data[:current])
+	end
+end
