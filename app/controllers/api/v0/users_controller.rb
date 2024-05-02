8a3bacb44a27ef2ae5fc9342672c7608322d8a16
Matt Darlington <matthewsdarlington@gmail.com>
2024-05-02 11:07:51 -0600

    Add specific exception handling to give users more detailed info about what might have gone wrong

--- app/controllers/api/v0/users_controller.rb
+++ app/controllers/api/v0/users_controller.rb
@@ -1,30 +1,46 @@
 class Api::V0::UsersController < ApplicationController 
 	def create
 		user = User.new(user_params)
-		if user
-			if user.save
-				render json: { data: user_data(user) }, status: :created
-			else
-				render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
-			end
+		user.generate_api_key
+		
+		if user.save && user.authenticate(user_params[:password])
+			render json: { data: user_data(user) }, status: 201
+		elsif user.errors[:email].include?("can't be blank")
+			render json: { error: "Please provide a valid email address" }, status: :bad_request
+		elsif user.errors[:email].include?("has already been taken")
+			render json: { error: "The email provided is already linked with an account" }, status: :bad_request
+		elsif user.errors[:email].include?("format is invalid")
+			render json: { error: "A valid email and password is required" }, status: :bad_request
 		else
-			render json: { error: "We could not find a valid user in the system"}, status: :unprocessable_entity
+			render json: { error: "A valid email and password is required" }, status: :bad_request
 		end
 	end
 
 	def login
 		user = User.find_by(email: params[:email])
-		if user && user.authenticate(params[:password])
-			render json: { data: user_data(user) }, status: 200
+		if user.present?
+			if user.authenticate(params[:password])
+				render json: { data: user_data(user) }, status: 200
+			elsif user.errors[:email].include?("can't be blank")
+				render json: { error: "Email can't be blank"}, status: 
+			elsif user.errors[:email].include?("doesn't match email")
+				render json: { error: "Valid email and password are required"}
+			elsif user.errors[:password].include?("doesn't match Password")
+				render json: { error: "Valid email and password are required"}
+			elsif user.errors[:password].include?("can't be blank")
+				render json: { error: "Password can't be blank"}
+			else
+				render json: { error: "There was a problem validating the email or password provided."}
+			end
 		else
-			render json: { error: "The credentials entered were not accepted. Please try again." }, status: :unprocessable_entity
+			render json: { error: "User credential error: We could not find a user with the email/username combination used." }
 		end
 	end
 
 	private
 
 	def user_params
-		params.require(:user).permit(:email, :password, :password_confirmation)
+		params.require(:user).permit(:email, :password, :password_confirmation, :api_key)
 	end 
 
 	def user_data(user)
