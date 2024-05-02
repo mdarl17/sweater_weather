915c4103e5b8c1d51d1067ba65eff7c29623674b
Matt Darlington <matthewsdarlington@gmail.com>
2024-05-02 11:07:08 -0600

    Remove logic for arrival time and weather on arrival to road trip facade

--- app/controllers/api/v0/road_trips_controller.rb
+++ app/controllers/api/v0/road_trips_controller.rb
@@ -1,24 +1,7 @@
 class Api::V0::RoadTripsController < ApplicationController
 	def index
-		from = params[:from]
-		to = params[:to]
-
-		response = RoadTripsService.new.get_travel_time(from, to)
-		duration = response[:route][:formattedTime]
-
-		def dest_time(duration)
-			t1 = Time.parse(duration)
-			Time.now + t1.hour * 3600 + t1.min * 60
-		end
-
-		dest_weather = WeatherFacade.new(q: from).weather.hourly_weather[dest_time(duration).hour]
-
-		road_trip = {
-			from: params[:from],
-			to: params[:to],
-			trip_time: duration,
-			arrival_weather: "#{dest_weather[:temperature]}, #{dest_weather[:conditions]}"
-		}
+		facade = RoadTripsFacade.new(origin: params[:origin], destination: params[:destination])
+		road_trip = facade.trip_details
 
 		render json: {
 			data: {
