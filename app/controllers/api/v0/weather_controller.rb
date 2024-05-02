0958349fc9654628deb298b9d9981be7b018c9ad
Matt Darlington <matthewsdarlington@gmail.com>
2024-05-02 11:09:22 -0600

    Add exception handling, along with searching weather api using lat/lon coords instead of city name

--- app/controllers/api/v0/weather_controller.rb
+++ app/controllers/api/v0/weather_controller.rb
@@ -1,8 +1,13 @@
 class Api::V0::WeatherController < ApplicationController
 	def forecast
-		w_facade = WeatherFacade.new(q: params[:q], days: params[:days])
-		weather = w_facade.weather
-
-		render json: WeatherSerializer.new(weather)
+		facade = WeatherFacade.new(location: params[:q], days: params[:days])
+		geo_response = facade.geo_coords
+		latlon = "#{geo_response.split(",")[0]},#{geo_response.split(",")[1]}"
+		weather_response = WeatherFacade.new(location: latlon, days: params[:days]).weather
+		if weather_response
+			render json: WeatherSerializer.new(weather_response)
+		else
+			render error: weather_response.errors
+		end
 	end
 end
