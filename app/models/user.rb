83b04c9729ccd00b005eb0b6659a18528ec779ab
Matt Darlington <matthewsdarlington@gmail.com>
2024-05-02 11:17:23 -0600

    Remove api_key presence validation since it isn't needed for user login or registration (api_key created at registration though); but used to confirm any weather or location search is being made by authenticated user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,11 @@
 class User < ApplicationRecord 
 	validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
 	validates :password, presence: true, confirmation: true
+	validates :api_key, uniqueness: true
 
 	has_secure_password
 
-	before_create :generate_api_key
-
-	private
-
 	def generate_api_key
-		self.api_key = SecureRandom.hex(12)
+		self.api_key = SecureRandom.hex(13)
 	end
 end
