83c976feb111627c24bdda5bb51d616af8d37d95
Matt Darlington <matthewsdarlington@gmail.com>
2024-05-02 11:21:31 -0600

    Update defs to use @origin and @destination to avoid confusion with initial data request

--- app/services/road_trips_service.rb
+++ app/services/road_trips_service.rb
@@ -1,15 +1,48 @@
 class RoadTripsService
-	def get_travel_time(from, to)
-		url = "https://www.mapquestapi.com""/directions/v2/route"
+	def initialize(origin, destination)
+		@origin = origin
+		@destination = destination
+	end
 
-		response = Faraday.get(url) do |f|
-			f.headers["Content-Type"] = "application/json"
-			f.headers["Accept"] = "application/json"
-			f.params[:from] = from
-			f.params[:to] = to
-			f.params[:key] = Rails.application.credentials.mapquest[:api_key]
-		end
-		
+	def get_travel_time
+		response = mq_conn.get( "/directions/v2/route")
 		JSON.parse(response.body, symbolize_names: true)
 	end
+
+	def get_time_zones
+		response = weather_conn(@origin).get("/v1/current.json")
+		parsed = JSON.parse(response.body, symbolize_names: true)
+		origin_tz = parsed[:loscation][:tz_id]
+
+		response = weather_conn(@destination).get("/v1/current.json")
+		parsed = JSON.parse(response.body, symbolize_names: true)
+		destination_tz = parsed[:location][:tz_id]
+			{
+				origin_tz: origin_tz,
+				destination_tz: destination_tz
+			}
+	end
+
+	def mq_conn
+		Faraday.new(url: "https://www.mapquestapi.com") do |f|
+			f.headers["Content-Type"] = "application/json"
+			f.headers["Accept"] = "application/json"
+			f.params[:from] = @origin
+			f.params[:to] = @destination
+			f.params[:key] = Rails.application.credentials.mapquest[:api_key]
+		end
+	end
+
+	def weather_conn(location)
+		Faraday.new(url: "https://api.weatherapi.com") do |f|
+			f.headers = {
+				"Content-Type": "application/json",
+				"Accept": "application/json"
+			}
+			f.params[:key] = Rails.application.credentials.weather[:api_key]
+			f.params[:q] = location
+		end
+	end
+
+	
 end
