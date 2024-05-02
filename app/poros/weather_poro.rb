0bbab5ec4190b4bce4fb65af5dba3b8b02471ac1
Matt Darlington <matthewsdarlington@gmail.com>
2024-05-02 11:20:18 -0600

    Move current_weather hash to array to make consistent with five_day and hourly weather defs

--- app/poros/weather_poro.rb
+++ app/poros/weather_poro.rb
@@ -5,20 +5,24 @@ class WeatherPoro
 	def initialize(attrs)
 		@id = "null"
 		@type = "forecast"
-		@current_weather = {
-			last_updated: attrs[:real_time][:current][:last_updated],
-			temperature: attrs[:real_time][:current][:temp_f],
-			feels_like: attrs[:real_time][:current][:feels_like_f],
-			humidity: attrs[:real_time][:current][:humidity],
-			uvi: attrs[:real_time][:current][:uv],
-			visibility: attrs[:real_time][:current][:vis_miles],
-			condition: {
-				text: attrs[:real_time][:current][:condition][:text],
-				icon: attrs[:real_time][:current][:condition][:icon]
-			}
-		}
+		@current_weather = current_array(attrs[:real_time]) if attrs[:real_time]
 		@daily_weather = five_day_array(attrs[:daily]) if attrs[:daily]
 		@hourly_weather = hourly_array(attrs[:hourly]) if attrs[:hourly]
+	end
+
+	def current_array(attrs)
+		{
+			last_updated: attrs[:current][:last_updated],
+				temperature: attrs[:current][:temp_f],
+				feels_like: "#{attrs[:current][:feelslike_f]}°F",
+				humidity: attrs[:current][:humidity],
+				uvi: attrs[:current][:uv],
+				visibility: attrs[:current][:vis_miles],
+				condition: {
+					text: attrs[:current][:condition][:text],
+					icon: attrs[:current][:condition][:icon]
+				}
+			}
 	end
 
 	def five_day_array(days_array)
@@ -36,14 +40,20 @@ class WeatherPoro
 	end
 
 	def hourly_array(hours_array)
-		hours_array.map do |h|
-			{
-				time: h[:time].split(" ").second,
-				temperature: h[:temp_f],
-				conditions: h[:condition][:text],
-				icon: h[:condition][:icon]
-			}
+
+		hours_array.map do |day_array|
+			day_array.map do |d|
+				{
+					time: "#{Time.parse(d[:time]).strftime("%-l:%M")}#{am_or_pm(Time.parse(d[:time]).hour)}",
+					temperature: "#{d[:temp]}°F",
+					condition: d[:condition].strip,
+				}
+			end
 		end
+	end
+
+	def am_or_pm(hour)
+		hour < 12 ? "AM" : "PM"
 	end
 
 	def get_state_code(state)
