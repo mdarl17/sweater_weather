071ecd2d1a8382b7c8a47dba18aab2979c6130d8
Matt Darlington <matthewsdarlington@gmail.com>
2024-05-02 21:20:09 -0600

    New assertions for #current, #hourly, #daily weather hashes; new tests for #geo_coords; combine weather facade tests with duplicate assertions on facade[:current] attribute

--- spec/facades/weather_facade_spec.rb
+++ spec/facades/weather_facade_spec.rb
@@ -12,9 +12,9 @@ RSpec.describe WeatherFacade, :vcr, type: :facade do
 	end
 
 	describe "#initialize" do 
-		it "is instantiated with a new WeatherService instance, and given location and air quality attributes" do 
+		it "exists" do
 			expect(@facade).to be_a WeatherFacade
-			expect(@facade.instance_variables).to match_array( [:@geo_coords, :@location, :@service])
+			expect(@facade.instance_variables).to match_array([:@location, :@service, :@geo_coords, :@days])
 			expect(@facade.service).to be_a WeatherService
 			expect(@facade.location).to be_a Hash
 			expect(@facade.location[:location]).to be_a String
@@ -24,42 +24,36 @@ RSpec.describe WeatherFacade, :vcr, type: :facade do
 		end
 	end
 
-	describe "#forecast" do 
-		it "returns current weather data of a given location (city, state)" do
+	describe "WeatherFacade Attributes" do
+		it "has some fixed attributes" do
 			expect(@weather).to be_a WeatherPoro
 			expect(@weather.instance_variables).to match_array([:@current_weather, :@daily_weather, :@hourly_weather, :@id, :@type])
 			expect(@weather.id).to eq("null")
 			expect(@weather.type).to eq("forecast")
-			expect(@weather.current_weather).to be_a Hash
-			expect(@weather.current_weather.keys).to match_array(
-				[:last_updated, :temperature, :feels_like, :humidity, :uvi, :visibility, :condition]
-			)
-			expect(@weather.current_weather[:last_updated]).to be_a String
-			expect(@weather.current_weather[:temperature]).to be_a String
-			expect(@weather.current_weather[:feels_like]).to (be_nil).or (be_a String)
-			expect(@weather.current_weather[:humidity]).to be_an Integer
-			expect(@weather.current_weather[:uvi]).to be_a Float
-			expect(@weather.current_weather[:visibility]).to be_a Float
-			expect(@weather.current_weather[:condition]).to be_a Hash
-			expect(@weather.current_weather[:condition].keys).to match_array([:text, :icon])
-			expect(@weather.current_weather[:condition][:text]).to be_a String
-			expect(@weather.current_weather[:condition][:icon]).to be_a String
-		end
-	end
-
-	describe "#geo_coords" do 
-		it "takes a city location and returns the latitude/longitude coordinates of that city" do 
-			expect(WeatherFacade.new("Denver, CO").geo_coords).to be_a String
-			expect(WeatherFacade.new("Denver, CO").geo_coords).to eq("39.74001,-104.99202")
 		end
 	end
 
 	describe "#current_weather" do 
 		it "given latitude and longitude of a location, it returns the current weather conditions of that location" do
-			cw = WeatherFacade.new("Denver, CO").current_weather
-			
-			expect(WeatherFacade.new("Denver, CO").geo_coords).to eq("39.74001,-104.99202")
+			wf = WeatherFacade.new("Denver, CO")
+			cw = wf.current_weather
+
+			expect(cw).to be_a Hash
 			expect(cw.keys).to eq([:location, :current])
+
+			expect(cw[:current][:last_updated]).to be_a String
+			expect(cw[:current][:temp_f]).to be_a Float
+			expect(cw[:current][:feelslike_f]).to be_a Float
+			expect(cw[:current][:humidity]).to be_an Integer
+			expect(cw[:current][:uv]).to be_a Float
+			expect(cw[:current][:vis_miles]).to be_a Float
+			expect(cw[:current][:condition]).to be_a Hash
+			expect(cw[:current][:condition].keys).to eq([:text, :icon, :code])
+			expect(cw[:current][:condition][:text]).to be_a String
+			expect(cw[:current][:condition][:icon]).to be_a String
+			expect(cw[:current][:condition][:code]).to be_an Integer
+			expect(cw[:current][:last_updated]).to be_a String
+
 			expect(cw[:location]).to be_a Hash
 			expect(cw[:location].keys).to eq([:name, :region, :country, :lat, :lon, :tz_id, :localtime_epoch, :localtime])
 			expect(cw[:location][:name]).to be_a String
@@ -76,6 +70,81 @@ RSpec.describe WeatherFacade, :vcr, type: :facade do
 			expect(cw[:location][:tz_id]).to eq("America/Denver")
 			expect(cw[:location][:localtime_epoch]).to be_an Integer
 			expect(cw[:location][:localtime]).to be_a String
+		end
+	end
+
+	describe "#daily_weather" do 
+		it "returns weather forecasts for the number of days requested (default is 5 days)" do
+			wf = WeatherFacade.new("Louisville, KY")
+			dw = wf.daily_weather
+			dw_10 = wf.daily_weather(10)
+
+			expect(dw.count).to eq(5)
+			expect(dw_10.count).to eq(10)
+
+			expect(dw.first.keys).to eq([:date, :date_epoch, :day, :astro, :hour])
+
+			expect(dw[0][:date]).to eq(Time.now.strftime("%Y-%m-%d"))
+			expect(dw[1][:date]).to eq((Time.now + 1*24*3600).strftime("%Y-%m-%d"))
+			expect(dw[2][:date]).to eq((Time.now + 2*24*3600).strftime("%Y-%m-%d"))
+			expect(dw[3][:date]).to eq((Time.now + 3*24*3600).strftime("%Y-%m-%d"))
+			expect(dw[4][:date]).to eq((Time.now + 4*24*3600).strftime("%Y-%m-%d"))
+
+			expect(dw[0][:hour].count).to eq(24)
+			expect(dw[0][:hour].first).to include(:condition)
+
+			expect(dw[0][:day][:maxtemp_f]).to be_a Float
+			expect(dw[0][:day][:mintemp_f]).to be_a Float
+			expect(dw[0][:day][:avgtemp_f]).to be_a Float
+		end
+	end
+
+	describe "#hourly_weather" do 
+		it "returns the weather forecast for every hour for a given number of days (default is 5 days)" do
+			wf = WeatherFacade.new("Louisville, KY")
+			hw = wf.hourly_weather
+			hw_10 = wf.hourly_weather(10)
+
+			expect(hw.count).to eq(5)
+			expect(hw_10.count).to eq(10)
+
+			expect(hw.first.count).to eq(24)
+			expect(hw.sample.sample.keys).to eq([:date, :time, :temp, :condition])
+			expect(hw.sample.sample[:date]).to be_a String
+			expect(hw.sample.sample[:time]).to be_a String
+			expect(hw.sample.sample[:temp]).to be_a Float
+			expect(hw.sample.sample[:condition]).to be_a String
+		end
+	end
+
+	describe "#weather" do 
+		it "returns curren, hourly, and daily weather forecasts together" do
+			wf = WeatherFacade.new("New York, NY", 3)
+			weather = wf.weather
+
+			expect(weather).to be_a WeatherPoro
+			expect(weather.instance_variables).to eq([:@id, :@type, :@current_weather, :@daily_weather, :@hourly_weather])
+			expect(weather.current_weather.keys).to eq([:last_updated, :temperature, :feels_like, :humidity, :uvi, :visibility, :condition])
+			expect(weather.daily_weather).to be_an Array
+			expect(weather.daily_weather.count).to eq(3)
+			expect(weather.daily_weather.first).to be_a Hash
+			expect(weather.daily_weather.first.keys).to eq([:date, :sunrise, :sunset, :max_temp, :min_temp, :condition, :icon])
+			expect(weather.hourly_weather).to be_an Array
+			expect(weather.hourly_weather.count).to eq(72) # 24 hrs * 3 days
+			expect(weather.hourly_weather.first).to be_a Hash
+			expect(weather.hourly_weather.first.keys).to eq([:date, :time, :temperature, :condition])
+			expect(weather.hourly_weather.first[:date]).to be_a String
+			expect(weather.hourly_weather.first[:time]).to be_a String
+			expect(weather.hourly_weather.first[:temperature]).to be_a String
+			expect(weather.hourly_weather.first[:temperature]).to include("°F")
+			expect(weather.hourly_weather.first[:condition]).to be_a String
+		end
+	end
+
+	describe "#geo_coords" do 
+		it "takes a city location and returns the latitude/longitude coordinates of that city" do 
+			expect(WeatherFacade.new("Denver, CO").geo_coords).to be_a String
+			expect(WeatherFacade.new("Denver, CO").geo_coords).to eq("39.74001,-104.99202")
 		end
 	end
 end 
