80934a3694452ad79d47f5d2295512d25cbddba0
Matt Darlington <matthewsdarlington@gmail.com>
2024-05-05 00:38:20 -0600

    Remove #current_user from application controller since no session created for API service; valid api_key determines whether user gets api service or not

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,2 @@
 class ApplicationController < ActionController::API
-	before_action :current_user
-	# helper_method :current_user
-	
-	def current_user
-		user = User.find_by(email: params[:email])
-
-		if user && user.authenticate(user.password)
-			require 'pry'; binding.pry
-			@_current_user = user
-		else
-			user = nil
-		end
-	end
 end
