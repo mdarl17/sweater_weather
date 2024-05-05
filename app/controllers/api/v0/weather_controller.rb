931ddc47d69cc44640d14a9c910397a00658a82d
Matt Darlington <matthewsdarlington@gmail.com>
2024-05-04 23:40:19 -0600

    Remove #geo_coords from weather facade, do all location/lat_lon coords conversion in locations facade (#lat_lon

--- app/controllers/api/v0/weather_controller.rb
+++ app/controllers/api/v0/weather_controller.rb
@@ -1,9 +1,9 @@
 class Api::V0::WeatherController < ApplicationController
 	def forecast
-		facade = WeatherFacade.new(location: params[:q], days: params[:days])
-		geo_response = facade.geo_coords
-		latlon = "#{geo_response.split(",")[0]},#{geo_response.split(",")[1]}"
-		weather_response = WeatherFacade.new(location: latlon, days: params[:days]).weather
+		loc_hash = LocationsFacade.new(params[:location]).lat_lon
+		latlon = "#{loc_hash[:lat]},#{loc_hash[:lng]}"
+		facade = WeatherFacade.new(latlon, params[:days])
+		weather_response = facade.weather
 
 		if weather_response
 			render json: WeatherSerializer.new(weather_response)
