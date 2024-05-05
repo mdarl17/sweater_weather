d9382d43ba5e73423277d5489d1077e9a97acdf3
Matt Darlington <matthewsdarlington@gmail.com>
2024-05-04 23:36:06 -0600

    Add password authentication to current_user in app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,15 @@
 class ApplicationController < ActionController::API
 	before_action :current_user
 	# helper_method :current_user
-
-	private
+	
 	def current_user
-		@_current_user = User.find_by(email: params[:email])
+		user = User.find_by(email: params[:email])
+
+		if user && user.authenticate(user.password)
+			require 'pry'; binding.pry
+			@_current_user = user
+		else
+			user = nil
+		end
 	end
 end
