bd9130cb3c4c65df174e63e68fbc08d05a0c0e06
Matt Darlington <matthewsdarlington@gmail.com>
2024-05-04 23:38:15 -0600

    Swap save and authenticate in users#create since it shouldn't save until successful auth; add exception handling for password not in system

--- app/controllers/api/v0/users_controller.rb
+++ app/controllers/api/v0/users_controller.rb
@@ -2,9 +2,10 @@ class Api::V0::UsersController < ApplicationController
 	def create
 		user = User.new(user_params)
 		user.generate_api_key
-		
-		if user.save && user.authenticate(user_params[:password])
+		if user.authenticate(user_params[:password]) && user.save
 			render json: { data: user_data(user) }, status: 201
+		elsif user.errors[:password].include?("doesn't match Password")
+			render json: { error: "A valid email and password is required" }
 		elsif user.errors[:email].include?("can't be blank")
 			render json: { error: "Please provide a valid email address" }, status: :bad_request
 		elsif user.errors[:email].include?("has already been taken")
@@ -21,14 +22,16 @@ class Api::V0::UsersController < ApplicationController
 		if user.present?
 			if user.authenticate(params[:password])
 				render json: { data: user_data(user) }, status: 200
+			elsif !user.authenticate(params[:password])
+				render json: { error: "Valid email and password are required" }
 			elsif user.errors[:email].include?("can't be blank")
-				render json: { error: "Email can't be blank"}, status: 
+				render json: { error: "Email can't be blank" }, status: 
 			elsif user.errors[:email].include?("doesn't match email")
-				render json: { error: "Valid email and password are required"}
+				render json: { error: "Valid email and password are required" }
 			elsif user.errors[:password].include?("doesn't match Password")
-				render json: { error: "Valid email and password are required"}
+				render json: { error: "Valid email and password are required" }
 			elsif user.errors[:password].include?("can't be blank")
-				render json: { error: "Password can't be blank"}
+				render json: { error: "Password can't be blank" }
 			else
 				render json: { error: "There was a problem validating the email or password provided."}
 			end
@@ -41,7 +44,7 @@ class Api::V0::UsersController < ApplicationController
 
 	def user_params
 		params.require(:user).permit(:email, :password, :password_confirmation, :api_key)
-	end 
+	end
 
 	def user_data(user)
 		{
