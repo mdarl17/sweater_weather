a1866cd96291150757d44a1bd0b9c28ebce39300
Matt Darlington <matthewsdarlington@gmail.com>
2024-05-05 17:36:25 -0600

    Add api_key param/arg needed to verify user has been authenticated

--- app/controllers/api/v0/road_trips_controller.rb
+++ app/controllers/api/v0/road_trips_controller.rb
@@ -1,6 +1,6 @@
 class Api::V0::RoadTripsController < ApplicationController
 	def index
-		facade = RoadTripsFacade.new(origin: params[:origin], destination: params[:destination])
+		facade = RoadTripsFacade.new(origin: params[:origin], destination: params[:destination], api_key: params[:api_key])
 		road_trip = facade.trip_details
 
 		render json: {
