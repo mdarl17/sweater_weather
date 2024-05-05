336c49b67fa6570a0cd48c78b340fd33e38d5803
Matt Darlington <matthewsdarlington@gmail.com>
2024-05-05 17:34:45 -0600

    no change

--- app/controllers/api/v0/locations_controller.rb
+++ app/controllers/api/v0/locations_controller.rb
@@ -3,7 +3,7 @@ class Api::V0::LocationsController < ApplicationController
 		facade = LocationsFacade.new(params[:location])
 		result = facade.lat_lon
 			raise ArgumentError, "A valid location must be provided" if params[:location].scan(/\d/).length > 0
-			raise SecurityError, "The user's email was not included in this request" unless current_user && params[:email] == current_user.email
+			# raise SecurityError, "The user's email was not included in this request" unless current_user && params[:email] == current_user.email
 			raise SecurityError, "The user's api_key was not included in this request" unless params[:api_key] == current_user.api_key
 			render json: { "lat": result[:lat], "lon": result[:lng] } 
 		end
