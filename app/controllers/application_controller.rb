2cefcc5a06959af4ddc000a2c7daae8d53483719
Matt Darlington <matthewsdarlington@gmail.com>
2024-05-05 17:16:25 -0600

    New exception handlers for ArgumentErrors (invalid location provided) and SecurityErrors (invalid email or api_key provided)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,24 @@
 class ApplicationController < ActionController::API
-end
+	rescue_from ArgumentError, with: :handle_errors
+	rescue_from SecurityError, with: :handle_errors
+	
+	def handle_errors(exception)
+		status_code = case exception
+									when ArgumentError
+										:bad_request
+									when SecurityError
+										:unauthorized
+									else
+										:unprocessable_entity
+									end
+		render json: { error: exception.message }, status: status_code
+	end
+		
+		def current_user
+			if params[:email]
+				@_current_user = User.find_by(email: params[:email])
+			elsif user_params[:email]
+				@_current_user = User.find_by(email: user_params[:email])
+			end
+		end
+	end
