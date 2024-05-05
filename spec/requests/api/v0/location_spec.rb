22343816ba1f084df0e6a21883ca6812bb780f01
Matt Darlington <matthewsdarlington@gmail.com>
2024-05-05 00:36:21 -0600

    Add happy path for locations controller returning valid, accurate lat/lon data for city/region provided

--- spec/requests/api/v0/location_spec.rb
+++ spec/requests/api/v0/location_spec.rb
@@ -2,10 +2,15 @@ require "rails_helper"
 
 RSpec.describe "Get geo data", :vcr, type: :request do 
 	describe "Api::V0::Location" do 
-		it "searches for and returns geo data for a given city and state/region" do
-			location = "cincinnati,oh"
+		it "it returns latitude and longitude data for a given city, state, or region" do
 			get "/api/v0/locations", headers: {"Content-Type": "application/json", "Accept": "application/json "}, 
-					params: { location: location }
+					params: { location: "90,-43" }
+			
+			parsed = JSON.parse(response.body, symbolize_names: true)
+			expect(response).to have_http_status(200)
+			expect(response.body).to be_a String
+			expect(parsed).to be_a Hash
+			expect(parsed).to eq({:lat=>39.10713, :lon=>-84.50413})
 		end
 	end
 end 
